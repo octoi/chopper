71001f18fce111101275920b39369a9a80a143aa
Fadhil <fadhilsaheer@gmail.com>
2023-07-31 21:56:48 +0530

    added name field

--- app/account/register/page.tsx
+++ app/account/register/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
 
 export default function RegisterPage() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -16,6 +17,14 @@ export default function RegisterPage() {
     <form onSubmit={handleFormSubmit}>
       <h1 className='text-3xl font-semibold'>Register</h1>
       <p className='mt-2'>Create an account on Chopper</p>
+      <Input
+        type='text'
+        placeholder='John Doe'
+        className='mt-5 w-full'
+        required
+        value={name}
+        setValue={setName}
+      />
       <Input
         type='email'
         placeholder='john@email.com'
