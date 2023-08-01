8de7be90f6f0b8fb4f456dff9fcbbd58d6ea349e
Fadhil <fadhilsaheer@gmail.com>
2023-08-01 22:16:31 +0530

    feat: register mutation

--- app/account/register/page.tsx
+++ app/account/register/page.tsx
@@ -3,14 +3,26 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { trpc } from '@/lib/trpc';
 
 export default function RegisterPage() {
+  const registerMutation = trpc.user.register.useMutation();
+
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleFormSubmit = (e: any) => {
     e.preventDefault();
+
+    registerMutation.mutate({
+      username,
+      name,
+      password,
+      profile: encodeURI(
+        `https://avatars.dicebear.com/api/identicon/${username}.svg`
+      ),
+    });
   };
 
   return (
@@ -19,19 +31,19 @@ export default function RegisterPage() {
       <p className='mt-2'>Create an account on Chopper</p>
       <Input
         type='text'
+        placeholder='johndoe'
+        className='mt-5 w-full'
+        required
+        value={username}
+        setValue={setUsername}
+      />
+      <Input
+        type='text'
         placeholder='John Doe'
         className='mt-5 w-full'
         required
         value={name}
         setValue={setName}
-      />
-      <Input
-        type='email'
-        placeholder='john@email.com'
-        className='mt-5 w-full'
-        required
-        value={email}
-        setValue={setEmail}
       />
       <Input
         type='password'
